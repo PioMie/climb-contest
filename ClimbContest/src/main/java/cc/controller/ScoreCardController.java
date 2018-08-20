f819c89dba759f1802cf6e8570fa2e3e851e2211
Michal F <mfipoczta@gmail.com>
2018-08-20 19:58:15 +0200

    1. Slight modification to index.html - syntax corrected, sorting scripts and jquery added 2. login class added 3. new js files added (Datatables, jquery)

--- ClimbContest/src/main/java/cc/controller/ScoreCardController.java
+++ ClimbContest/src/main/java/cc/controller/ScoreCardController.java
@@ -1,8 +1,11 @@
 package cc.controller;
 
+//import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+//import java.util.Comparator; //Added to make sorting possible
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +20,21 @@ public class ScoreCardController {
 
 	@GetMapping("/")
 	public ModelAndView home(Map<String, Object> model) {
+		
+		 
+		
 		List<Climber> climbers = Arrays.asList(new Climber("Kozak", 5, 90, 85), new Climber("Dzik", 4, 21, 90),
 				new Climber("Anakin", 4, 24, 78), new Climber("Zdzich", 3, 30, 98), new Climber("Marta", 1, 20, 62));
 
+		
+		
 		model.put("climbers", climbers);
+//		model.put("climbersSortedByName", climbersSortedByName);
+//		model.put("climbersSortedByScore", climbersSortedByScore);
+//		model.put("climbersSortedByAge", climbersSortedByAge);		
+//		model.put("climbersSortedByWeight", climbersSortedByWeight);
 		model.put("header", "Score Card");
-
+		
 		return new ModelAndView("index", model);
 	}
 
