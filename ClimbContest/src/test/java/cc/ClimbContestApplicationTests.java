95985409b304b8a83eaf8684aafd96a07decdca2
Piotr Mierzejewski <piotr.mierzejewski@sap.com>
2018-09-22 23:40:57 +0200

    Fixing failing sample empty test

--- ClimbContest/src/test/java/cc/ClimbContestApplicationTests.java
+++ ClimbContest/src/test/java/cc/ClimbContestApplicationTests.java
@@ -1,5 +1,6 @@
 package cc;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,5 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ClimbContestApplicationTests {
 
 	// TODO: general tests....
-
+	@Test
+	public void exampleTest() {
+		// TODO sample
+	}
 }
